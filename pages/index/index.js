ec11d754bc18f7d7656951ad92832170707f5a50
helion <czy1996@users.noreply.github.com>
2017-07-31 11:38:10 +0800

    zanui

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,8 @@ var app = getApp()
 var util = require('../../utils/util.js')
 var {Api} = require('../../utils/api.js')
 
+import { $wuxButton } from '../../wux/components/wux'
+
 var book = new Api('book')
 
 console.log(util)
@@ -12,7 +14,35 @@ console.log(util)
 Page({
     data: {
         motto: 'Hello World',
-        books: {}
+        books: {},
+        inputShowed: false,
+        inputVal: "",
+        tab: {
+            list: [{
+                id: '1',
+                title: '最新商品1'
+            }, {
+                id: '2',
+                title: '最新商品2'
+            }, {
+                id: '3',
+                title: '最新商品3'
+            }, {
+                id: '4',
+                title: '最新商品4'
+            }, {
+                id: '5',
+                title: '最新商品5'
+            }, {
+                id: '6',
+                title: '最新商品6'
+            }],
+            selectedId: '1',
+            scroll: true,
+            height: 45
+        },
+        index: 3,
+        opened: !1,
     },
     //事件处理函数
     bindViewTap: function () {
@@ -30,14 +60,75 @@ Page({
                 books: data
             })
         })
+        this.initButton()
     },
 
-    getBooksAll: function (cb) {
-        wx.request({
-            url: util.api + "book/all",
-            success: res => {
-                cb(res.data)
-            }
+    initButton(position = 'bottomRight') {
+        this.setData({
+            opened: !1,
         })
+
+        this.button = $wuxButton.init('br', {
+            position: position,
+
+
+            buttons: [
+                {
+                    label: 'View on Github',
+                    icon: "",
+                },
+                {
+                    label: 'View on About',
+                    icon: "",
+                },
+                {
+                    label: 'View on Demo',
+                    icon: "",
+                }
+            ],
+            buttonClicked(index, item) {
+                index === 0 && wx.showModal({
+                    title: 'Thank you for your support!',
+                    showCancel: !1,
+                })
+
+                index === 1 && wx.switchTab({
+                    url: '/pages/about/index'
+                })
+
+                index === 2 && wx.switchTab({
+                    url: '/pages/index/index'
+                })
+
+                return true
+            },
+            callback(vm, opened) {
+                vm.setData({
+                    opened,
+                })
+            },
+        })
+    },
+
+    showInput: function () {
+        this.setData({
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
     }
 })
