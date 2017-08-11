8f4a6689f29c7d21fb9577aed31258763387d900
helion <czy1996@users.noreply.github.com>
2017-08-11 10:44:18 +0800

    scan page

--- pages/scan/scan.js
+++ pages/scan/scan.js
@@ -1,3 +1,21 @@
 /**
  * Created by nicai on 2017/8/11.
  */
+var {log} = require('../../utils/util.js')
+
+Page({
+    data: {
+        isbn: ''
+    },
+
+    scan(){
+        wx.scanCode({
+            success: (res) => {
+                log(res)
+            },
+            fail: () => {
+                log('scan fail')
+            }
+        })
+    }
+});
